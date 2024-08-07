a0aec2395ae25b0a557d1751a88c64ec8ce29bd3
Elena <elenaharan@gmail.com>
2024-08-07 08:28:50 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,7 @@
+const {createServer} = require('http');
+
+const server = createServer ((req, res) => {
+	res.end("Hello world!")
+})
+
+server.listen(8000, ()=> console.log("Listening for requests on port 8000"))
