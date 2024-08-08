755da4ec46778f6606f297c572eed9c815e5263e
Elena <elenaharan@gmail.com>
2024-08-08 12:55:41 +0100

    create server

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const {createServer} = require('http');
+const { readFileSync } = require("fs");
+const { createServer } = require("http");
+const { parse } = require("url");
 
-const server = createServer ((req, res) => {
-	res.end("Hello world!")
-})
+const htmlTemplate = readFileSync(`${__dirname}/index.html`, "utf-8");
 
-server.listen(8000, ()=> console.log("Listening for requests on port 8000"))
+const server = createServer((req, res) => {
+  const pathName = parse(req.url, true).pathname;
+
+  if (pathName === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    res.end(htmlTemplate);
+  } else if (pathName === "/test") {
+    res.end("Test");
+  } else {
+    res.end("The url cannot be found");
+  }
+});
+
+server.listen(8000, () => console.log("Listening for requests on port 8000"));
