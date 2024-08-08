68905aa935786737da303389e7328ae19a998496
Elena <elenaharan@gmail.com>
2024-08-08 13:30:57 +0100

    add starter file

--- starter.js
+++ starter.js
@@ -1,8 +1,62 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://github.com/jonasschmedtmann/ultimate-react-course/blob/main/20-manual-ssr/starter/starter.js</string>
-</dict>
-</plist>
+const pizzas = [
+  {
+    name: "Focaccia",
+    price: 6,
+  },
+  {
+    name: "Pizza Margherita",
+    price: 10,
+  },
+  {
+    name: "Pizza Spinaci",
+    price: 12,
+  },
+  {
+    name: "Pizza Funghi",
+    price: 12,
+  },
+  {
+    name: "Pizza Prosciutto",
+    price: 15,
+  },
+];
+
+function Home() {
+  return (
+    <div>
+      <h1>🍕 Fast React Pizza Co.</h1>
+      <p>This page has been rendered with React on the server 🤯</p>
+
+      <h2>Menu</h2>
+      <ul>
+        {pizzas.map((pizza) => (
+          <MenuItem
+            pizza={pizza}
+            key={pizza.name}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  return (
+    <div>
+      <button onClick={() => setCount((c) => c + 1)}>+1</button>
+      <span>{count}</span>
+    </div>
+  );
+}
+
+function MenuItem({ pizza }) {
+  return (
+    <li>
+      <h4>
+        {pizza.name} (${pizza.price})
+      </h4>
+      <Counter />
+    </li>
+  );
+}
